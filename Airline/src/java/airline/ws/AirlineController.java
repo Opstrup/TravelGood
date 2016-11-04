3d7c247000ddbc7f620458d8813efcf4682792ff
Anders Opstrup <aopstrup@gmail.com>
2016-11-04 15:47:51 +0100

    added some more stub code for the airline service

--- Airline/src/java/airline/ws/AirlineController.java
+++ Airline/src/java/airline/ws/AirlineController.java
@@ -1,5 +1,13 @@
 package airline.ws;
 
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+
+@WebService(serviceName = "AirlineService")
 public class AirlineController {
     
+    @WebMethod(operationName = "getFlights")
+    public String getFlights() {
+        return "Hello World!";
+    }
 }
---
+++ Airline/src/java/airline/ws/Flight.java
@@ -0,0 +1,15 @@
+package airline.ws;
+
+import java.util.Date;
+
+public class Flight {
+    private String _destination;
+    private Date _date;
+    
+    public Flight(String des, Date date) {
+        _destination = des;
+        _date = date;
+    }
+    
+    
+}
