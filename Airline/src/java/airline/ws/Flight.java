3ee29bf21b1adbada4a2301ba8051d588996559b
lucacambiaghi <lc.cambiaghi@gmail.com>
2016-11-07 14:28:28 +0100

    Changed carrierName in airlineName

--- Airline/src/java/airline/ws/Flight.java
+++ Airline/src/java/airline/ws/Flight.java
@@ -8,15 +8,15 @@ public class Flight {
     private String destinationAirport;
     private Date departureDate;
     private Date arrivalDate;
-    private String carrierName;
+    private String airlineName;
     private int availableSeats;
 
-    public Flight(String startAirport, String destinationAirport, Date departureDate, Date arrivalDate, String carrierName, int availableSeats) {
+    public Flight(String startAirport, String destinationAirport, Date departureDate, Date arrivalDate, String airlineName, int availableSeats) {
         this.startAirport = startAirport;
         this.destinationAirport = destinationAirport;
         this.departureDate = departureDate;
         this.arrivalDate = arrivalDate;
-        this.carrierName = carrierName;
+        this.airlineName = airlineName;
         this.availableSeats = availableSeats; //when booked, availableSeats -= 1
     }
 
@@ -36,8 +36,8 @@ public class Flight {
         return arrivalDate;
     }
 
-    public String getCarrierName() {
-        return carrierName;
+    public String getAirlineName() {
+        return airlineName;
     }
 
     public int getAvailableSeats() {
