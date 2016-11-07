7007d396d5a8217b47205f5512904f8493afe325
lucacambiaghi <lc.cambiaghi@gmail.com>
2016-11-07 14:44:12 +0100

    changed bookingNumber to be randomly chosen instead of it being hardcoded

--- Airline/src/airline/ws/FlightInformation.java
+++ Airline/src/airline/ws/FlightInformation.java
@@ -1,13 +1,16 @@
 package airline.ws;
 
+import java.util.Random;
+
 public class FlightInformation {
     private int bookingNumber;
     private int flightPrice;
     private String reservationAgency;
     private airline.ws.Flight flight;
 
+    //bookngNumber is kept as parameter for testing purposes
     public FlightInformation(int bookingNumber, int flightPrice, String airlineName, airline.ws.Flight flight) {
-        this.bookingNumber = bookingNumber;
+        this.bookingNumber = new Random().nextInt(50) +1;
         this.flightPrice = flightPrice;
         this.reservationAgency = airlineName;
         this.flight = flight;
