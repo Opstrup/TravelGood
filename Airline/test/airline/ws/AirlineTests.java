049ffc56ec812f931238eb6b23e9f170d4110c83
lucacambiaghi <lc.cambiaghi@gmail.com>
2016-11-07 14:29:37 +0100

    Implemented getFlights() test

--- Airline/test/airline/ws/AirlineTests.java
+++ Airline/test/airline/ws/AirlineTests.java
@@ -1,18 +1,42 @@
 package airline.ws;
 
-import java.util.Date;
+import java.util.*;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class AirlineTests {
-    
-    /*
-     * Simepl unit test of the flight class 
-     */
+public class AirlineTests extends airline.ws.AirlineModel{
+
+    public AirlineTests(){
+        this.flightInformationDB = new ArrayList<airline.ws.FlightInformation>();
+
+        Date departureDate = new GregorianCalendar(2014, Calendar.FEBRUARY, 11).getTime();
+        Date arrivalDate = (Date) departureDate.clone();
+
+        airline.ws.Flight testFlight = new airline.ws.Flight("Paris", "Copenhagen", departureDate, arrivalDate, "Ryanair", 10);
+        airline.ws.FlightInformation testFlightInfo = new airline.ws.FlightInformation(1, 100, "Lameduck", testFlight);
+        this.flightInformationDB.add(testFlightInfo);
+    }
+
+
     @Test
-    public void creationOfAFlightTest() {
-        Date depature = new Date(2016, 11, 10);
-        Flight UUT = new Flight("Paris", "Copenhagen", depature, 10);
-        assertEquals(UUT.getDestination(), "Paris");
+    public void getFlightsTest() {
+
+        AirlineTests model = new AirlineTests();
+
+        Date departureDate = new GregorianCalendar(2014, Calendar.FEBRUARY, 11).getTime();
+        Date arrivalDate = (Date) departureDate.clone();
+
+        airline.ws.Flight expectedFlight = new airline.ws.Flight("Paris", "Copenhagen", departureDate, arrivalDate, "Ryanair", 10);
+        airline.ws.FlightInformation expectedFlightInfo = new airline.ws.FlightInformation(1, 100, "Lameduck", expectedFlight);
+
+        List<airline.ws.FlightInformation> actualResult = model.getFlights("Paris", "Copenhagen", departureDate);
+
+        assert actualResult.size() == 1;
+
+        airline.ws.FlightInformation actualFlightInfo = actualResult.get(0);
+
+        assertEquals(expectedFlightInfo.getBookingNumber(), actualFlightInfo.getBookingNumber());
+
     }
 }
