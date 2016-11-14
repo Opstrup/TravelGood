3b8316f50185c0f141c6c01d4b97c4f6f6ec2e00
Vitali <vitali-p@hotmail.com>
2016-11-14 01:37:30 +0100

    REST client created, and test performed.

---
+++ TravelGoodRESTClient/src/travelgoodrestclient/TravelGoodRESTClient.java
@@ -0,0 +1,16 @@
+package travelgoodrestclient;
+
+/**
+ *
+ * @author Vitali
+ */
+public class TravelGoodRESTClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
