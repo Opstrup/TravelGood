d62f5fd69d565d06cfe35b02aa193eb399752937
Andreas Hjorth <arhjorth@hotmail.com>
2016-11-18 13:25:45 +0100

    Niceview bookHotel (using fastmoney)

--- Hotel_NiceView/src/java/hotel/ws/HotelModel.java
+++ Hotel_NiceView/src/java/hotel/ws/HotelModel.java
@@ -31,6 +31,30 @@ public class HotelModel {
         return resultList;
     }
     
+    public boolean bookHotel(int bookingNumber, CreditCardInfoType ccInfo) throws CreditCardFaultMessage{
+        for(HotelInformation hotelInfo : hotelInformationDB){
+            if(hotelInfo.getBookingNumber() == bookingNumber){
+                if(hotelInfo.needsCreditCardGuarantee()){
+                    return validateCreditCard(07, ccInfo, bookingNumber);
+                }else{
+                    return true;
+                }
+            }
+        }
+        return false;       
+    }
     
+ //   The bookHotel operation takes a booking number and credit card information (depending
+ //   on whether a credit card guarantee is required or not) and books the hotel. If a guarantee is required, the
+ //   operation validateCreditCard from the Bank service is called. The bookHotel operation returns true, if
+ //   the booking was successful, and returns a fault (i.e., throws an exception) if the credit card information
+ //   was not valid, there was not enough money on the client account, or if for other reasons the booking
+ //   fails.
+
+    private static boolean validateCreditCard(int group, hotel.ws.CreditCardInfoType creditCardInfo, int amount) throws CreditCardFaultMessage {
+        hotel.ws.BankService service = new hotel.ws.BankService();
+        hotel.ws.BankPortType port = service.getBankPort();
+        return port.validateCreditCard(group, creditCardInfo, amount);
+    }
     
 }
