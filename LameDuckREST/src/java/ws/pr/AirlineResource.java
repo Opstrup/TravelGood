b119404e62b6a9ad44b7a53b57d611e2483d3fec
QPC\Vitali <vitali-p@hotmail.com>
2016-11-18 15:12:11 +0100

    Added current java files to RESTLame Duck project

--- LameDuckREST/src/java/ws/pr/AirlineResource.java
+++ LameDuckREST/src/java/ws/pr/AirlineResource.java
@@ -1,4 +1,4 @@
-package ws.pr;
+package ws.travelgood;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -6,30 +6,44 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
 
 /**
+ * REST Web Service
  * @author Vitali
+ * Service description: The airliner service provides 
+ *  flight information, booking and canceling of the flight.
  */
-@Path("airline")
+@Path("lameDuck")
 public class AirlineResource {
-    
-    @Context
-    private UriInfo context;
-    
+   
+     /**
+     * Retrieves representation of an instance.
+     * @return an instance of java.lang.String
+     */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces("text/plain")
     public String getFlights(){
         return "Airline data of your request are...";
     }
     @POST
+    @Produces("text/plain")
     public boolean bookFlight () {
         //Booking number..
         return true;
     }
-    @GET
+    
+    /**
+     * PUT method for updating or creating an instance of AirlineResource
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Produces("text/plain")
     public String cancleFlight () {
+        //Update booking.
         return "Returning 50% of the booking by ID.";
     }
     
--- LameDuckREST/src/java/ws/pr/ApplicationConfig.java
+++ LameDuckREST/src/java/ws/pr/ApplicationConfig.java
@@ -1,4 +1,4 @@
-package ws.pr;
+package ws.travelgood;
 
 import java.util.Set;
 import javax.ws.rs.core.Application;
@@ -6,9 +6,9 @@ import javax.ws.rs.core.Application;
 /**
  * @author Vitali
  */
-@javax.ws.rs.ApplicationPath("webresources") //("webresources")
+@javax.ws.rs.ApplicationPath("webresources") //http://localhost8080/pr/webresources/airline//("webresources")
 public class ApplicationConfig extends Application {
-
+    
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new java.util.HashSet<>();
@@ -23,7 +23,8 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(ws.pr.AirlineResource.class);
+        resources.add(ws.travelgood.AirlineResource.class);
+        resources.add(ws.travelgood.ItinerarieResource.class);
     }
     
 }
