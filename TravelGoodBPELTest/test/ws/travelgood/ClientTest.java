865e5d1d6e0b4dce5d2add27d83abff75fc0c9d2
Luca Cambiaghi <lc.cambiaghi@gmail.com>
2016-11-18 16:29:47 +0100

    First working prototype of TravelGoodBPEL. It creates an empty itinerary with ID 1. Test is working

--- TravelGoodBPELTest/test/ws/travelgood/ClientTest.java
+++ TravelGoodBPELTest/test/ws/travelgood/ClientTest.java
@@ -11,21 +11,28 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
+import org.netbeans.j2ee.wsdl.travelgoodbpel.src.travelgoodbpel.ItineraryType;
 
 public class ClientTest {
-    
 
     @Test
     public void should_return_empty_itinerary(){
-        //
+        ItineraryType itinerary = createItinerary("create my itinerary");
+        assertNotNull(itinerary);
     }
     
-    private static String createItineray() {
-        ws.travelgood.TravelGoodBPELService service = new ws.travelgood.TravelGoodBPELService();
-        ws.travelgood.TravelGoodBPELPortType port = service.getTravelGoodBPELPort();
-        return port.createItineray();
+    @Test
+    public void should_return_empty_itinerary_with_id(){
+        ItineraryType itinerary = createItinerary("create my itinerary");
+        String id = itinerary.getItineraryID();
+        assertEquals("1", id);
     }
     
+    private static ItineraryType createItinerary(java.lang.Object createItineraryReq) {
+        org.netbeans.j2ee.wsdl.travelgoodbpel.src.travelgoodbpel.TravelGoodBPELService service = new org.netbeans.j2ee.wsdl.travelgoodbpel.src.travelgoodbpel.TravelGoodBPELService();
+        org.netbeans.j2ee.wsdl.travelgoodbpel.src.travelgoodbpel.TravelGoodBPELPortType port = service.getTravelGoodBPELPort();
+        return port.createItinerary(createItineraryReq);
+    }
+
 }
 
