4776eb1f02bfd924fe2262f76269bd159d3c6597
Anders Opstrup <aopstrup@gmail.com>
2016-11-20 12:42:51 +0100

    more clean up of the itinerarie

--- TravelGoodREST/src/java/ws/rs/ItinerarieResource.java
+++ TravelGoodREST/src/java/ws/rs/ItinerarieResource.java
@@ -4,17 +4,11 @@ import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
-/**
- * @author Vitali
- */
 @Path("Itinerarie")
 public class ItinerarieResource {
     
     @GET
-//    @Produces(MediaType.APPLICATION_JSON)
     @Produces("application/json")
     public Itinerary createIntinerarie() {
         Itinerary newIntinerary = new Itinerary(5);
