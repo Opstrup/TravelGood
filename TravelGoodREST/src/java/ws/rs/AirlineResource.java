a80064062a5db829640095dc885f68a4cb01e4f3
Anders Opstrup <aopstrup@gmail.com>
2016-11-21 13:53:41 +0100

    stated on the imp with airline

--- TravelGoodREST/src/java/ws/rs/AirlineResource.java
+++ TravelGoodREST/src/java/ws/rs/AirlineResource.java
@@ -1,5 +1,9 @@
 package ws.rs;
 
+import airline.ws.FlightInformation;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -15,8 +19,12 @@ public class AirlineResource {
 
     @GET
     @Produces("application/json")
-    public String getFlights(){
-        return "Airline data of your request are...";
+    public List<FlightInformation> getFlights () {
+        String startAirport = "copenhagen";
+        String endAirport = "rome";
+        Date departureDate = new GregorianCalendar(2016, 8, 10).getTime();
+        Date startDate = new GregorianCalendar(2016, 8, 10).getTime();
+        return getFlights_1(startAirport, endAirport, departureDate);
     }
     
     @POST
@@ -29,6 +37,11 @@ public class AirlineResource {
     @Produces("text/plain")
     public String cancleFlight () {
         return "Returning 50% of the booking by ID.";
+    }    
+
+    private static java.util.List<airline.ws.FlightInformation> getFlights_1(java.lang.String startAirport, java.lang.String endAirport, javax.xml.datatype.XMLGregorianCalendar startDate) {
+        airline.ws.AirlineService service = new airline.ws.AirlineService();
+        airline.ws.AirlineController port = service.getAirlineControllerPort();
+        return port.getFlights(startAirport, endAirport, startDate);
     }
-    
 }
